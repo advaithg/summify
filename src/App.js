0e7be14800fcec14538b30ee63714ff14b0ca501
nareh <53708656+nareha@users.noreply.github.com>
2021-03-27 11:14:13 -0700

    Added some initial stylish

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default class App extends React.Component {
   }
   
   handleSubmit(event) {
-    alert('An essay was submitted: ' + this.state.value);
+    window.scrollTo(0, document.body.scrollHeight);
     fetch('/time').then(res => res.json()).then(data => {
       this.setState({summaryText: data.summary});
       this.setState({isLoading: false});
@@ -35,34 +35,36 @@ export default class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        <header className="AppHeader">
-          <img src={logo} className="AppLogo" alt="logo" />
-          <h1>Title of Project Here</h1>
-        </header>
-        <section className="AppBody">
-          <p>
-            Summarize long lectures by copying and pasting the transcript in the textbox below!
-          </p>
-          <textarea 
-            className = "TextArea"
-            value={this.state.value} 
-            onChange={this.handleChange}
-            placeholder = "Paste text to be summarized here."
-          />
-          <button className="Button" onClick={this.handleSubmit}>Submit</button>
-          {this.state.isLoading &&
-          <section>
+      <div className="Background">
+        <div className="App">
+          <header className="AppHeader">
             <img src={logo} className="AppLogo" alt="logo" />
+            <h1>Title of Project Here</h1>
+          </header>
+          <section className="AppBody">
+            <p>
+              Summarize long lectures by copying and pasting the transcript in the textbox below!
+            </p>
+            <textarea 
+              className = "TextArea"
+              value={this.state.value} 
+              onChange={this.handleChange}
+              placeholder = "Paste text to be summarized here."
+            />
+            <button className="Button" onClick={this.handleSubmit}>Submit</button>
+            {this.state.isLoading &&
+            <section>
+              <img src={logo} className="AppLogo" alt="logo" style={{marginTop: "100px"}} />
+            </section>
+            }
+            {this.state.summaryText !== '' &&
+            <section className="Summary">
+              <h4 style={{textAlign:"left"}}>Summary:</h4>
+              <p style={{fontSize: "large"}}>{this.state.summaryText}</p>
+            </section>
+            }
           </section>
-          }
-          {this.state.summaryText !== '' &&
-          <section className="Summary">
-            <h4 style={{textAlign:"left"}}>Summary:</h4>
-            <p style={{fontSize: "large"}}>{this.state.summaryText}</p>
-          </section>
-          }
-        </section>
+        </div>
       </div>
     );
   }
