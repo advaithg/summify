1c442f1b9ffd3070fbbb715c9d51285c42f6983a
MuhenderRaj <rkmr02@gmail.com>
2021-03-27 11:13:11 +0530

    Textarea and submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      value: 'Paste text to be summarized here.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Title of Project Here</h1>
+          <p>
+            Summarize long lectures by copying and pasting the transcript in the textbox below!
+          </p>
+          <textarea 
+            value={this.state.value} 
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>submit</button>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
